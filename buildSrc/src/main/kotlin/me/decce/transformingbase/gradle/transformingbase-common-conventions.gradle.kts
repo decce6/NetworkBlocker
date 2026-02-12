190d7601a68235184dc41c8dd11a41ef0dfdff4b
decce6 <decce6@proton.me>
2026-02-12 11:22:08 +0800

    Correct side requirement information

--- buildSrc/src/main/kotlin/me/decce/transformingbase/gradle/transformingbase-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/me/decce/transformingbase/gradle/transformingbase-common-conventions.gradle.kts
@@ -165,7 +165,7 @@ publishMods {
     curseforge {
         accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
         clientRequired = true
-        serverRequired = true
+        serverRequired = false
         projectId = "1450738"
         projectSlug = modid
         if (hasProperty("minecraft_supported_from")) {
