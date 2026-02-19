fff9e22e70b054d59b203c2bd296e2db65e330fb
decce6 <decce6@proton.me>
2026-02-19 15:05:34 +0800

    Add new method for acquiring IMPL_LOOKUP

--- core/src/main/java/me/decce/transformingbase/core/util/ReflectionHelper.java
+++ core/src/main/java/me/decce/transformingbase/core/util/ReflectionHelper.java
@@ -1,6 +1,7 @@
 package me.decce.transformingbase.core.util;
 
 import sun.misc.Unsafe;
+import sun.reflect.ReflectionFactory;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -8,17 +9,27 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class ReflectionHelper {
-    public static final MethodHandles.Lookup LOOKUP;
+    public static final MethodHandles.Lookup LOOKUP = getImplLookup();
 
-    static {
+    public static MethodHandles.Lookup getImplLookup() {
         try {
-            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-            theUnsafe.setAccessible(true);
-            Unsafe unsafe = (Unsafe) theUnsafe.get(null);
-            Field implLookup = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
-            LOOKUP = (MethodHandles.Lookup) unsafe.getObject(unsafe.staticFieldBase(implLookup), unsafe.staticFieldOffset(implLookup));
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
+            var ctor = ReflectionFactory.getReflectionFactory()
+                    .newConstructorForSerialization(MethodHandles.Lookup.class, MethodHandles.Lookup.class.getDeclaredConstructor(Class.class));
+            var lookup = (MethodHandles.Lookup) ctor.newInstance(MethodHandles.Lookup.class);
+            return (MethodHandles.Lookup) lookup.findStaticGetter(MethodHandles.Lookup.class, "IMPL_LOOKUP", MethodHandles.Lookup.class).invokeExact();
+        }
+        catch (Throwable throwable) {
+            try {
+                Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+                theUnsafe.setAccessible(true);
+                Unsafe unsafe = (Unsafe) theUnsafe.get(null);
+                Field implLookup = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
+                return (MethodHandles.Lookup) unsafe.getObject(unsafe.staticFieldBase(implLookup), unsafe.staticFieldOffset(implLookup));
+            }
+            catch (Throwable throwable1) {
+                throwable1.addSuppressed(throwable);
+                throw new RuntimeException(throwable1);
+            }
         }
     }
 
