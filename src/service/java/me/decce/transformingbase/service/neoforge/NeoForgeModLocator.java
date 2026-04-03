739a3dde1ba86af1e4252a7377397a2c17f54a15
decce6 <decce6@proton.me>
2026-04-04 06:31:26 +0800

    Fix JiJ discovery on 1.21.1
    This commit fixes the `java.nio.file.ProviderNotFoundException: Provider "jar" not found` errors.

--- src/service/java/me/decce/transformingbase/service/neoforge/NeoForgeModLocator.java
+++ src/service/java/me/decce/transformingbase/service/neoforge/NeoForgeModLocator.java
@@ -2,30 +2,42 @@ package me.decce.transformingbase.service.neoforge;
 
 //? if neoforge {
 /*import me.decce.transformingbase.constants.Constants;
-import net.neoforged.neoforgespi.ILaunchContext;
+import net.neoforged.fml.loading.moddiscovery.locators.JarInJarDependencyLocator;
+import net.neoforged.fml.loading.moddiscovery.readers.JarModsDotTomlModFileReader;
+import net.neoforged.neoforgespi.locating.IDependencyLocator;
 import net.neoforged.neoforgespi.locating.IDiscoveryPipeline;
-import net.neoforged.neoforgespi.locating.IModFileCandidateLocator;
-import net.neoforged.neoforgespi.locating.IncompatibleFileReporting;
-import net.neoforged.neoforgespi.locating.ModFileDiscoveryAttributes;
+import net.neoforged.neoforgespi.locating.IModFile;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Objects;
 
-public class NeoForgeModLocator implements IModFileCandidateLocator {
+//? if <1.21.9 {
+/^import cpw.mods.jarhandling.SecureJar;
+^///?}
+
+public class NeoForgeModLocator extends JarInJarDependencyLocator implements IDependencyLocator {
     @Override
-    public void findCandidates(ILaunchContext context, IDiscoveryPipeline pipeline) {
+    public void scanMods(List<IModFile> loadedMods, IDiscoveryPipeline pipeline) {
         //? if <1.21.9 {
         /^try {
-            final var resource = this.getClass().getResource("/META-INF/jarjar/");
+            var secureJar = SecureJar.from(Path.of(NeoForgeModLocator.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
+            var resource = NeoForgeModLocator.class.getResource("/META-INF/jarjar/");
             try (var stream = Files.walk(Path.of(Objects.requireNonNull(resource).toURI()), 1)) {
                 stream.filter(path -> path.getFileName().toString().endsWith("-mod.jar"))
-                        .forEach(path -> pipeline.addJarContent(cpw.mods.jarhandling.JarContents.of(path), ModFileDiscoveryAttributes.DEFAULT, IncompatibleFileReporting.ERROR));
+                        .forEach(path -> {
+                            var modFile = IModFile.create(secureJar, JarModsDotTomlModFileReader::manifestParser);
+                            var mod = loadModFileFrom(modFile, Path.of("META-INF", "jarjar", path.getFileName().toString()), pipeline).orElseThrow();
+                            pipeline.addModFile(mod);
+                        });
             }
         } catch (Throwable t) {
             throw new RuntimeException("Loading " + Constants.MOD_ID + " JiJ mod", t);
         }
-        ^///?}
+        ^///?} else {
+        super.scanMods(loadedMods, pipeline);
+        //?}
     }
 }
 *///?}
